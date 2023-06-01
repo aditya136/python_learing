df1a5c59ce9e2d64f86af7d03a6be9823477f67f
aditya136 <adityabanerjee102@gmail.com>
2023-06-01 22:14:00 +0600

    learning-data-type(text/str, int, floating, complex)

--- practice.py
+++ practice.py
@@ -21,3 +21,35 @@
 #else: 
 #    print("Your 1st answer is wrong. The correct one is- ", 23 + 16)
 #This code doesn't work as I want....I guess I need a bit more knowledge😊
+
+
+#Learning about Data Types-
+#complex data type
+#z = 2j
+#print(type(z))
+
+#int type data
+#aditya = 136
+#print(aditya)
+#print(type(aditya))
+
+#floating type data
+#aditya = 136.6
+#print(aditya)
+#print(type(aditya))
+
+#complex type data
+#aditya = 23482977j
+#print(aditya)
+#print(type(aditya))
+
+
+#text type data/str type data
+#MyName = "Aditya Banerjee"
+#print(MyName)
+#print(type(MyName))
+
+#your_name = "I don't know"
+#print(MyName + your_name)
+
+#print("My name is" + " " + MyName)
